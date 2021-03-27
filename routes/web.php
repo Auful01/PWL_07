e2ed466b8ae396b32348570481a512007b597b1c
aufulkirom-id <aufulkirom-id>
2021-03-27 22:52:20 +0700

    PWL_07

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+
+// use App\Http\Controllers\ContacController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\WelcomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route::get('/home', [IndexController::class, 'index'])->name('index.home');
+// Route::get('/about', [AboutController::class, 'about'])->name('index.about');
+// Route::get('/blog', [BlogController::class, 'blog'])->name('index.blog');
+// Route::resource('contact', ContactController::class);
+
+// Route::get('/', [HomeController::class,'index'])->name('index.home');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
